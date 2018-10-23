360fc317adf310c6a1b425ffd566fdb221be444a
Wind Shin <johnbaram@naver.com>
2018-10-23 21:12:23 +0900

    commit for task 4 and bestCutoff value is set to 88

--- src/test/java/edu/handong/csee/pp1/dp/CLAMITest.java
+++ src/test/java/edu/handong/csee/pp1/dp/CLAMITest.java
@@ -67,19 +67,19 @@ public class CLAMITest {
 	@Test
 	public void testCLAMIWithApacheProjectToFindBestCutoffForPrecision() {
 		
-		// TASK5 TODO Use this method to find the best cutoff
-		
-		String bestCutoff = "50";
+		// TASK4 TODO Use this method to find the best cutoff		
+		String bestCutoff = "88";
 		
 		String[] args = {"-f","data/Apache.arff","-l","isDefective","-p", "TRUE", "-m", "-c", bestCutoff};
 
 		CLAMI.main(args);
+		
 	}
 	
 	@Test
 	public void testCLAMIWithApacheProjectToFindBestCutoffForFmeasure() {
 		
-		// TASK6 TODO Use this method to find the best cutoff for f-measure
+		// TASK5 TODO Use this method to find the best cutoff for f-measure
 		
 		String bestCutoff = "50";
 		
